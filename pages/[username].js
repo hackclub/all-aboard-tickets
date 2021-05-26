c84a38be65aec15d977d375c9ddd8bb53722830b
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-05-26 22:01:54 +0800

    tickets!

---
+++ pages/[username].js
@@ -0,0 +1,84 @@
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Grid,
+  Card,
+  Button,
+  Flex,
+  Image,
+  Link
+} from 'theme-ui'
+import Head from 'next/head'
+import Tilt from 'react-tilt'
+import Meta from '../components/meta'
+
+export default function App({ username, name }) {
+  return (
+    <Box
+      sx={{
+        backgroundColor: '#906b52',
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%23694e3c' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E")`
+      }}
+    >
+      <Meta
+        title={`${name} is going to this Saturday's secret Hack Club meeting.`}
+        description="...."
+        image={`https://tickers.hackclub.com/api/ticket?username=${username}`}
+      />
+      <Container variant="copy">
+        <Flex
+          sx={{
+            minHeight: '100vh',
+            justifyContent: 'center',
+            justifyItems: 'center',
+            alignItems: 'center',
+            alignContent: 'center',
+            color: 'white'
+          }}
+        >
+          <Box sx={{}}>
+            <Tilt
+              className="Tilt"
+              options={{ max: 7, perspective: 200 }}
+              style={{ width: '100%' }}
+            >
+              <Image
+                src={`/api/ticket?username=${username}`}
+                sx={{
+                  width: '100%',
+                  boxShadow: 'elevated',
+                  borderRadius: 'extra'
+                }}
+              />
+            </Tilt>
+            <Box sx={{ mt: 3, textAlign: 'center' }}>
+              <Button sx={{ mr: 3 }} as="a" href={`https://twitter.com/intent/tweet?text=Heading%20to%20a%20secret%20%40hackclub%20meeting%20this%20Saturday...%20I%20wonder%20what%20is%20happening...%20https%3A%2F%2Ftickets.hackclub.com%2F${username}`}>
+                Share on Twitter
+              </Button>
+              <Button as="a" download href={`/api/ticket?username=${username}`}>Download Ticket</Button>
+            </Box>
+          </Box>
+        </Flex>
+      </Container>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.0.3/tilt.jquery.min.js"></script>
+    </Box>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const { WebClient } = require('@slack/web-api')
+
+  const token = process.env.SLACK_TOKEN
+
+  const web = new WebClient(token)
+
+  let details = await web.users.info({
+    user: params.username
+  })
+  return {
+    props: { username: params.username, name: details.user.profile.real_name}
+  }
+}
---
+++ pages/api/ticket.js
@@ -0,0 +1,198 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import { parseRequest } from './_lib/parser'
+import { getScreenshot } from './_lib/chromium'
+import { getHtml } from './_lib/template'
+
+const isDev = !process.env.AWS_REGION
+const isHtmlDebug = false
+
+export default async (req, res) => {
+  console.log(req.query)
+  const { WebClient } = require('@slack/web-api')
+
+  const token = process.env.SLACK_TOKEN
+
+  const web = new WebClient(token)
+
+  let details = await web.users.info({
+    user: req.query.username
+  })
+
+  const AirtablePlus = require('airtable-plus')
+
+  const airtable = new AirtablePlus({
+    baseID: 'appoBQQwL8ABXjasg',
+    apiKey: process.env.AIRTABLE_TICKETS_TOKEN,
+    tableName: 'Slack Usernames'
+  })
+
+  let paths = await airtable.read()
+
+  let valid = false
+
+  let number
+
+  paths = paths.map((x, index) => {
+    if (x['fields']['Usernames'] == req.query.username) {
+      valid = true
+      number = index + 1
+    }
+    return {
+      username: x['fields']['Usernames'],
+      number: index
+    }
+  })
+
+  if (!valid) {
+    const res = await airtable.create({ 'Usernames': req.query.username });
+    paths = await airtable.read()
+    paths = paths.map((x, index) => {
+      if (x['fields']['Usernames'] == req.query.username) {
+        valid = true
+        number = index + 1
+      }
+      return {
+        username: x['fields']['Usernames'],
+        number: index
+      }
+    })
+  }
+  const palx = require('palx')
+  let colours = []
+  Object.entries(palx('#5F8893')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  Object.entries(palx('#85865F')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  Object.entries(palx('#927B97')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  Object.entries(palx('#AA7B78')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  Object.entries(palx('#906B52')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  Object.entries(palx('#CE9960')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  Object.entries(palx('#4E6447')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  Object.entries(palx('#A34730')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  Object.entries(palx('#5F8893')).map(x => {
+    if (x[0] == 'base' || x[0] == 'black') {
+      return
+    }
+    x[1].map((x, index) => {
+      if (index < 6) {
+        return
+      } else {
+        colours.push(x)
+      }
+    })
+  })
+  console.log(colours)
+  colours.reduce((unique, item) => {
+    return unique.includes(item) ? unique : [...unique, item]
+  }, [])
+  console.log(colours)
+  try {
+    const parsedReq = parseRequest(req)
+    const html = getHtml(parsedReq, details.user.profile.real_name, number, colours[number], details.user.profile.image_192)
+    if (isHtmlDebug) {
+      res.setHeader('Content-Type', 'text/html')
+      res.end(`<div style="width: 1200px; height: 630px">${html}</div>`)
+      return
+    }
+    const { fileType } = parsedReq
+    const file = await getScreenshot(html, fileType, isDev)
+    res.statusCode = 200
+    res.setHeader('Content-Type', `image/${fileType}`)
+    res.setHeader(
+      'Cache-Control',
+      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+    )
+    res.end(file)
+  } catch (e) {
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>')
+    console.error(e)
+  }
+}
